4a1bbd30cdfb7bd99182c9a003f112cea66bd757
Sandra90-16 <46812352+Sandra90-16@users.noreply.github.com>
2020-03-12 22:25:06 +0100

    Clases Java Peliculas 1

---
+++ Genero.java
@@ -0,0 +1,5 @@
+package Peliculas1;
+
+public enum Genero{DRAMA, COMEDIA, ACCION, TERROR, ROMANCE, AVENTURA, SCIFI}
+
+
---
+++ Trailer.java
@@ -0,0 +1,37 @@
+package Peliculas1;
+
+import java.net.URL;
+import java.sql.Time;
+
+public class Trailer {
+
+	private URL url;
+	private Time duracion;
+	
+
+	public URL getUrl() {
+		return url;
+	}
+
+
+	public void setUrl(URL url) {
+		this.url = url;
+	}
+
+
+	public Time getDuracion() {
+		return duracion;
+	}
+
+
+	public void setDuracion(Time duracion) {
+		this.duracion = duracion;
+	}
+
+
+	public Trailer() {
+		super();
+	
+	}
+
+}
